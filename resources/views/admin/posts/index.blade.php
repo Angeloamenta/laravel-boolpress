6981c3f8169d0977131f6f74ed48c274c8b397c1
Angeloamenta <angeloamenta7@gmail.com>
2022-03-01 17:07:21 +0100

    aggiunto delete

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -2,14 +2,37 @@
 
 @section('content')
 <div class="container">
-    <div class="col">
-        <div class="row">
+    <div class="row">
+        <div class="col">
+          @if (session('status'))
+          <div class="alert alert-danger">
+              {{ session('status') }}
+          </div>
+          @endif
             @foreach ($posts as $post)
             <div class="card w-100">
                 <div class="card-body">
                   <h5 class="card-title">{{$post['title']}}</h5>
                   <p class="card-text">{{$post['content']}}</p>
-                  <a href="{{ route('admin.posts.show', $post) }}" class="btn btn-primary">View</a>
+                  <div class="container">
+                    <div class="row">
+                      <div class="col-1">
+                        <a href="{{ route('admin.posts.show', $post) }}" class="btn btn-primary">View</a>
+
+                      </div>
+                      <div class="col-1">
+                        <a href="{{ route('admin.posts.edit', $post) }}" class="btn btn-primary">Edit</a>
+                        
+                      </div>
+                      <div class="col-1">
+                        <form action="{{ route('admin.posts.destroy', $post) }}" method="post">
+                          @csrf
+                          @method('DELETE')
+                          <input class="btn btn-danger" type="submit" value="Delete">             
+                        </form>          
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
             @endforeach
