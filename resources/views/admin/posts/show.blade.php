131b285657eda5bf40562ff4807cebec7044bdb2
Angeloamenta <angeloamenta7@gmail.com>
2022-03-02 15:14:42 +0100

    prova per prender enome user su show

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -7,7 +7,11 @@
             <div class="card w-100">
                 <div class="card-body">
                   <h5 class="card-title">{{$post->title}}</h5>
+                  {{-- prendo il nome della cateogria --}}
+                  <h6>{{$post->category()->first()->name}}</h6> 
                   <p class="card-text">{{$post->content}}</p>
+                  {{-- prendo il nome dell'user  --}}
+                  <p>{{$post->user()->first()->name}}</p>
                   {{-- <a href="" class="btn btn-primary">Button</a> --}}
                 </div>
               </div>
