a66dbb478f813c1777e186b213b4ea35afe95afe
Angeloamenta <angeloamenta7@gmail.com>
2022-03-02 16:20:13 +0100

    contenuto show.blade

--- resources/views/admin/categories/show.blade.php
+++ resources/views/admin/categories/show.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
-ciao
+
+<h1>
+    All posts of Category: {{ $category->name }}
+</h1>
+
+
+@foreach ($category->posts()->get() as $post)
+    <p>{{ $post->id }}</p>
+    <p>{{ $post->title }}</p>
+@endforeach 
+
+
 @endsection
