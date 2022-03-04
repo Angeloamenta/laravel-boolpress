cb9c1364cca7c5874c30e1a3bc6065dfcca8ab94
Angeloamenta <angeloamenta7@gmail.com>
2022-03-04 09:06:44 +0100

    spostato bottone delete category

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -17,14 +17,14 @@
                       <div class="col-1">
                         <a href="{{ route('admin.categories.show', $category) }}" class="btn btn-primary">View</a>
                       </div>
+                      <div class="col-1">
+                        <form action="{{ route('admin.categories.destroy', $category) }}" method="post">
+                          @csrf
+                          @method('DELETE')
+                          <input class="btn btn-danger" type="submit" value="Delete">             
+                        </form>          
+                      </div>
                     </div>
-                </div>
-                <div class="col-1">
-                  <form action="{{ route('admin.categories.destroy', $category) }}" method="post">
-                    @csrf
-                    @method('DELETE')
-                    <input class="btn btn-danger" type="submit" value="Delete">             
-                  </form>          
                 </div>
               </div>
             @endforeach
