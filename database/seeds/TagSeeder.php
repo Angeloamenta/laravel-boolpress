97a1f7233cbb27a3fe2de135ad257be35fbfd782
Angeloamenta <angeloamenta7@gmail.com>
2022-03-04 14:55:21 +0100

    compilato tagSeeder

--- database/seeds/TagSeeder.php
+++ database/seeds/TagSeeder.php
@@ -1,7 +1,8 @@
 <?php
+use Illuminate\Support\Str;
 
 use Illuminate\Database\Seeder;
-
+use App\Model\Tag;
 class TagSeeder extends Seeder
 {
     /**
@@ -11,6 +12,25 @@ class TagSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $tags = [
+            'animals',
+            'sport',
+            'game',
+            'photo',
+            'music',
+            'phone',
+            'web',
+            'Php',
+            'JS',
+            'HTML',
+            'Css'
+        ];
+
+        foreach ($tags as $tag) {
+            $newTag = new Tag();
+            $newTag->name = $tag;
+            $newTag->slug = Str::slug($tag, '-');
+            $newTag->save();
+        }
     }
 }
