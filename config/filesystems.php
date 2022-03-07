dd6f71a33b3c845738a21ac2e17357cfcc7a7063
Angeloamenta <angeloamenta7@gmail.com>
2022-03-07 14:02:55 +0100

    modificato filesystem

--- config/filesystems.php
+++ config/filesystems.php
@@ -12,8 +12,8 @@ return [
     | based disks are available to your application. Just store away!
     |
     */
-
-    'default' => env('FILESYSTEM_DRIVER', 'local'),
+    // cambiare in public
+    'default' => env('FILESYSTEM_DRIVER', 'public'),
 
     /*
     |--------------------------------------------------------------------------
