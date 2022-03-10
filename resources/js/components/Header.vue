b9d74fdb835eb436166200f922df3ffb07ccf19f
Angeloamenta <angeloamenta7@gmail.com>
2022-03-10 14:28:57 +0100

    vue non si visualizza

---
+++ resources/js/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <div class="row row-cols-1 row-cols-md-4 g-4">
+      <div class="col">
+        header
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// import Axios from "axios";
+
+  export default {
+    name: "Header",
+    data() {
+      return {
+        
+      }
+    },
+    created() {
+     
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- resources/js/front.js
+++ resources/js/front.js
@@ -6,16 +6,48 @@
 
 require('./bootstrap');
 
-window.Vue = require('vue');
+window.Vue = require('vue').default;
 
 import App from './views/App';
+import Home from './pages/Home';
+import About from './pages/About';
+import Products from './pages/Products';
+
+import VueRouter from 'vue-router';
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
+const router = new VueRouter ({
+    mode:'history',
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home
+        },
+        {
+            path: '/products',
+            name: 'products',
+            component: Products
+        },
+        {
+            path: '/contacts',
+            name: 'contacts',
+            component: Contacts
+        },
+        {
+            path: '/about',
+            name: 'about',
+            component: About
+        }
+    ]
+})
+
 const app = new Vue({
     el: '#app',
     render: h => h(App),
+    router
 });
